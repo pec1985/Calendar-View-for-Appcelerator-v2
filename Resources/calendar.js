e66e433545d17bd9a6b14a1406bf062870fc0fdf
Pedro Enrique <pedro.tma@gmail.com>
2010-11-24 13:01:23 -0500

    day3.5

--- Resources/calendar.js
+++ Resources/calendar.js
@@ -3,8 +3,8 @@ var win = Ti.UI.createWindow({backgroundImage:'pngs/Default-calinvite.png',layou
 //-------------------toolbar-------------------
 
 // title and buttons
-var nextMonth = Ti.UI.createImageView({right:15,width:12,height:15,title:'next',image:'pngs/next.png'});
-var prevMonth = Ti.UI.createImageView({left:15,width:12,height:15,title:'prev',image:'pngs/prev.png'});
+var nextMonth = Ti.UI.createButton({right:15,width:12,height:15,backgroundImage:'pngs/next.png'});
+var prevMonth = Ti.UI.createButton({left:15,width:12,height:15,backgroundImage:'pngs/prev.png'});
 var monthTitle = Ti.UI.createLabel({width:200,height:20,textAlign:'center',color:'#3a4756',font:{fontSize:20,fontWeight:'bold'}});
 
 var toolBar= Ti.UI.createView({top:0,width:322,height:44,backgroundImage:'pngs/monthgradient.png',layout:'vertical'});
@@ -32,26 +32,33 @@ toolBarDays.add(toolBarDays.saturday);
 // blah blah blah - this is also important
 toolBar.add(toolBarTitle);
 toolBar.add(toolBarDays);
+var oldDay;
+dayView = function(e){
+	var label = Ti.UI.createLabel({current:e.current,width:46,height:44,backgroundImage:'pngs/monthdaytile-Decoded.png',text:e.day,textAlign:'center',color:e.color,font:{fontSize:20,fontWeight:'bold'}});
+	if(e.day == e.dayOfMonth){
+		label.color='white';
+		label.backgroundImage='pngs/monthdaytiletoday_selected.png';
+		oldDay = label;
+	}
+	return label;
+};
 
 // -------------------main calendar function, this is EVERYTHING!! ha ha-------------------
 var calView = function(a,b,c){
-	var month = b;
-	var year = a;
-	if(b > 11){ month = b-12;year = a+1}
-	if(b < 0){ month = b+12;year = a-1}
-	
-	if(month==0){monthTitle.text='January';}
-	if(month==1){monthTitle.text='February';}
-	if(month==2){monthTitle.text='March';}
-	if(month==3){monthTitle.text='April';}
-	if(month==4){monthTitle.text='May';}
-	if(month==5){monthTitle.text='June';}
-	if(month==6){monthTitle.text='July';}
-	if(month==7){monthTitle.text='Augost';}
-	if(month==8){monthTitle.text='September';}
-	if(month==9){monthTitle.text='October';}
-	if(month==10){monthTitle.text='November';}
-	if(month==11){monthTitle.text='December';}
+	switch(b){
+		case 0: monthTitle.text='January '+a; break;
+		case 1: monthTitle.text='February '+a; break;
+		case 2: monthTitle.text='March '+a; break;
+		case 3: monthTitle.text='April '+a; break;
+		case 4: monthTitle.text='May '+a; break;
+		case 5: monthTitle.text='June '+a; break;
+		case 6: monthTitle.text='July '+a; break;
+		case 7: monthTitle.text='Augost '+a; break;
+		case 8: monthTitle.text='September '+a; break;
+		case 9: monthTitle.text='October '+a; break;
+		case 10: monthTitle.text='November '+a; break;
+		case 11: monthTitle.text='December '+a; break;
+	};
 	//create main calendar view
 	var mainView = Ti.UI.createView({layout:'horizontal',backgroundColor:'#DCDCDF',width:322,height:'auto',left:-1,top:0});
 	//set the time
@@ -61,29 +68,20 @@ var calView = function(a,b,c){
 	var daysInLastMonth = 32 - new Date(a,b-1,32).getDate();
 	var daysInNextMonth = (new Date(a,b,daysInMonth).getDay())-6;
 	//create day
-	dayView = function(e){
-		var label = Ti.UI.createLabel({current:e.current,width:46,height:44,backgroundImage:'pngs/monthdaytile-Decoded.png',text:e.day,textAlign:'center',color:e.color,font:{fontSize:20,fontWeight:'bold'}});
-		if(e.day == dayOfMonth){
-			label.color='white';
-			label.backgroundImage='pngs/monthdaytiletoday_selected.png';
-			oldDay = label;
-		}
-		return label;
-	};
 	//create a new empty day for custamization later - it will become first our current day
-	var oldDay = null;
+//	var oldDay;
 	//set initial day number
 	var dayNumber = daysInLastMonth-dayOfWeek+1;
 	//get last month's days
 	for(i=0;i<dayOfWeek;i++){
-		mainView.add(new dayView({day:dayNumber,color:'#8e959f',current:'no'}));
+		mainView.add(new dayView({day:dayNumber,color:'#8e959f',current:'no',dayOfMonth:''}));
 		dayNumber++;
 	};
 	// reset day number for current month
 	dayNumber = 1;
 	//get this month's days
 	for(i=0;i<daysInMonth;i++){
-		var newDay=new dayView({day:dayNumber,color:'#3a4756',current:'yes'});
+		var newDay=new dayView({day:dayNumber,color:'#3a4756',current:'yes',dayOfMonth:dayOfMonth});
 		mainView.add(newDay);
 		//the clicker function!!!! ha ha!
 	//	clicker(newDay,dayNumber);
@@ -93,7 +91,7 @@ var calView = function(a,b,c){
 	dayNumber = 1;
 	//get remaining month's days
 	for(i=0;i>daysInNextMonth;i--){
-		mainView.add(new dayView({day:dayNumber,color:'#8e959f',current:'no'}));
+		mainView.add(new dayView({day:dayNumber,color:'#8e959f',current:'no',dayOfMonth:''}));
 		dayNumber++;
 	};
 
@@ -111,7 +109,7 @@ var calView = function(a,b,c){
 			oldDay.backgroundPaddingLeft=0;
 			oldDay.backgroundPaddingBottom=0;
 			// set window title with day selected, for testing purposes only
-			win.title=monthTitle.text+' '+e.source.text+', '+year;
+			win.title=monthTitle.text+' '+e.source.text+', '+a;
 			// set characteristic of the day selected
 			if(e.source.text == dayOfMonth){
 				e.source.backgroundImage='pngs/monthdaytiletoday_selected.png';
@@ -156,6 +154,7 @@ nextMonth.addEventListener('click',function(){
 	win.remove(calendarView);
 	win.remove(shadow);
 	b++;
+	if(b > 11){ b = b-12; a++;}
 	calendarView = calView(a,b,c);
 	win.add(calendarView);
 	win.add(shadow);
@@ -165,7 +164,9 @@ prevMonth.addEventListener('click',function(){
 	win.remove(calendarView);
 	win.remove(shadow);
 	b--;
+	if(b < 0){ b = b+12;a--;}
 	calendarView = calView(a,b,c);
 	win.add(calendarView);
 	win.add(shadow);
 });
+
