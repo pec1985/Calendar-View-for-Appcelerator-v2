0de43df2db5ec85c157cc0d1a8d77cc0b9bf1b45
Pedro Enrique <pedro.tma@gmail.com>
2010-11-25 01:45:42 -0500

    day3

--- Resources/calendar.js
+++ Resources/calendar.js
@@ -1,5 +1,8 @@
 var win = Ti.UI.createWindow({backgroundImage:'pngs/Default-calinvite.png'});
 
+// button to close the window and go back
+var backButton = Ti.UI.createButton({bottom:20,height:40,width:200});
+
 //-------------------toolbar-------------------
 
 // title and buttons
@@ -33,34 +36,34 @@ toolBarDays.add(toolBarDays.saturday);
 toolBar.add(toolBarTitle);
 toolBar.add(toolBarDays);
 //create a new empty day for custamization later - it will become first our current day
-var oldDay;
 //create day
 dayView = function(e){
 	var label = Ti.UI.createLabel({current:e.current,width:46,height:44,backgroundImage:'pngs/monthdaytile-Decoded.png',backgroundColor:'#DCDCDF',text:e.day,textAlign:'center',color:e.color,font:{fontSize:20,fontWeight:'bold'}});
-	if(e.day == e.dayOfMonth){
-		label.color='white';
-		label.backgroundImage='pngs/monthdaytiletoday_selected.png';
-		oldDay = label;
-	}
 	return label;
+};
+
+monthName = function(e){
+	switch(e){
+		case 0: e='January'; break;
+		case 1: e='February'; break;
+		case 2: e='March'; break;
+		case 3: e='April'; break;
+		case 4: e='May'; break;
+		case 5: e='June'; break;
+		case 6: e='July'; break;
+		case 7: e='Augost'; break;
+		case 8: e='September'; break;
+		case 9: e='October'; break;
+		case 10: e='November'; break;
+		case 11: e='December'; break;
+	};
+	return e;
 };
 
 // -------------------main calendar function, this is EVERYTHING!! ha ha-------------------
 var calView = function(a,b,c){
-	switch(b){
-		case 0: monthTitle.text='January '+a; break;
-		case 1: monthTitle.text='February '+a; break;
-		case 2: monthTitle.text='March '+a; break;
-		case 3: monthTitle.text='April '+a; break;
-		case 4: monthTitle.text='May '+a; break;
-		case 5: monthTitle.text='June '+a; break;
-		case 6: monthTitle.text='July '+a; break;
-		case 7: monthTitle.text='Augost '+a; break;
-		case 8: monthTitle.text='September '+a; break;
-		case 9: monthTitle.text='October '+a; break;
-		case 10: monthTitle.text='November '+a; break;
-		case 11: monthTitle.text='December '+a; break;
-	};
+	var nameOfMonth = monthName(b);
+	monthTitle.text=nameOfMonth+' '+a;
 	//create main calendar view
 	var mainView = Ti.UI.createView({layout:'horizontal',width:322,height:'auto',top:44});
 	//set the time
@@ -82,6 +85,11 @@ var calView = function(a,b,c){
 	for(i=0;i<daysInMonth;i++){
 		var newDay=new dayView({day:dayNumber,color:'#3a4756',current:'yes',dayOfMonth:dayOfMonth});
 		mainView.add(newDay);
+		if(newDay.text == dayOfMonth){
+			newDay.color='white';
+			newDay.backgroundImage='pngs/monthdaytiletoday_selected.png';
+			var oldDay = newDay;
+		}
 		dayNumber++;
 	};
 	dayNumber = 1;
@@ -104,7 +112,7 @@ var calView = function(a,b,c){
 			oldDay.backgroundPaddingLeft=0;
 			oldDay.backgroundPaddingBottom=0;
 			// set window title with day selected, for testing purposes only
-			win.title=monthTitle.text+' '+e.source.text+', '+a;
+			backButton.title = nameOfMonth+' '+e.source.text+', '+a;
 			// set characteristic of the day selected
 			if(e.source.text == dayOfMonth){
 				e.source.backgroundImage='pngs/monthdaytiletoday_selected.png';
@@ -137,12 +145,14 @@ nextCalendarView.left=323;
 var prevCalendarView = calView(a,b-1,c,-323);
 prevCalendarView.left=-323;
 
+backButton.title=monthName(b)+' '+c+', '+a;
 
 // add everything to the window 
 win.add(toolBar);
 win.add(thisCalendarView);
 win.add(nextCalendarView);
 win.add(prevCalendarView);
+win.add(backButton);
 
 // yeah, open the window, why not?
 win.open({modal:true});
@@ -157,7 +167,6 @@ nextMonth.addEventListener('click',function(){
 	if(b > 11){ b = b-12; a++;}
 	b++;
 	thisCalendarView.animate(slideNext);
-
 	nextCalendarView.animate(slideReset);
 	setTimeout(function(){
 		thisCalendarView.left=-322;
@@ -175,17 +184,14 @@ prevMonth.addEventListener('click',function(){
 	if(b < 0){ b = b+12;a--;}
 	b--;
 	thisCalendarView.animate(slidePrev);
-
 	prevCalendarView.animate(slideReset);
-
 	setTimeout(function(){
 		thisCalendarView.left=322;
 		prevCalendarView.left=-1;
 		nextCalendarView = thisCalendarView;
 		thisCalendarView = prevCalendarView;
 		prevCalendarView = calView(a,b,c);
-		prevCalendarView.left=-322;
-	
+		prevCalendarView.left=-322;	
 		win.add(prevCalendarView);
 	},500);
 });
