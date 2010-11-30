0e4c9a73a7e61f4a2e21f2074888b7f1f2ac6291
Pedro Enrique <pedro.tma@gmail.com>
2010-11-29 20:14:32 -0500

    Version 3.2 beta (added days of the month)

--- Resources/calendar2.js
+++ Resources/calendar2.js
@@ -1,9 +1,9 @@
 var selectDate = function(element){
 	var thisWindow = Ti.UI.currentWindow;
-	var mainView = Ti.UI.createWebView({url:'pngs/index.html',height:304,bottom:-304});
+	var mainView = Ti.UI.createWebView({url:'pngs/index.html',height:324,bottom:-324});
 	win.add(mainView);
 	var slideUp =  Titanium.UI.createAnimation({bottom:0,duration:250});
-	var slideDown =  Titanium.UI.createAnimation({bottom:-304,duration:250});
+	var slideDown =  Titanium.UI.createAnimation({bottom:-324,duration:250});
 	var x = 0;
 	element.addEventListener('click',function(){
 		switch(x){
